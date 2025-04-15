94ac5438bf27899c0642f66e4c115eda4bc63154
rajkumar <raj4122002@gmail.com>
2025-04-15 12:38:45 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+// app.js
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('Hello from Kubernetes!'));
+app.listen(3000, () => console.log('Server is running on port 3000'));
